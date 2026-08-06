bb168ad8e397714cc73a6536672baa317e3fde15
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-08-06 07:07:20 +0530

    added a user model in backend folder

--- backend-folder/users/models.py
+++ backend-folder/users/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
+
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_online = models.BooleanField(default=False)
+    last_seen = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
