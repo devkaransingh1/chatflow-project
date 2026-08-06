f6d281b821bd7e6b2a85a1a6ec89c473ac13e47d
devkaransingh1 <2k24.csai1b.2413455@gmail.com>
2026-08-07 01:43:05 +0530

    setup the postgresql database for the project and created the signup API

--- backend-folder/config/settings.py
+++ backend-folder/config/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "chatflow_db",
+        "USER": "chatflow_user",
+        "PASSWORD": "your_strong_password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
